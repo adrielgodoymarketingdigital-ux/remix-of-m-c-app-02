b1bfb1f0e7fec44f8642f83020f365701e4d7e88
Adriel Godoy <adrielmktdigital01@gmail.com>
2026-06-29 22:37:03 -0300

    fix: grid de dispositivos sempre 2 colunas, 3 a partir de lg

--- src/components/dispositivos/GridDispositivos.tsx
+++ src/components/dispositivos/GridDispositivos.tsx
@@ -24,7 +24,7 @@ export function GridDispositivos({
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4">
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
       {dispositivos.map((dispositivo) => (
         <CardDispositivo
           key={dispositivo.id}
