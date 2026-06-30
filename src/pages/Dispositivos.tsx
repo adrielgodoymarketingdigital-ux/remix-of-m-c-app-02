47c540652668aafa0e536289858f21724ab96855
Adriel Godoy <adrielmktdigital01@gmail.com>
2026-06-29 22:28:07 -0300

    fix: ajustar grid de dispositivos para 2 colunas a partir de sm

--- src/pages/Dispositivos.tsx
+++ src/pages/Dispositivos.tsx
@@ -359,7 +359,7 @@ export default function Dispositivos() {
           </div>
 
           {loading ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
               {[1, 2, 3, 4].map((i) => (
                 <Skeleton key={i} className="h-[400px] w-full" />
               ))}
